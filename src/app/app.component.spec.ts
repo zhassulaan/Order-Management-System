d2cca4f24d0c2241b48e10f6932ca36c49cd0764
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-05-19 10:03:30 +0500

    name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'order-management-system' title`, () => {
+  it(`should have the 'Order-Management-System' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('order-management-system');
+    expect(app.title).toEqual('Order-Management-System');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, order-management-system');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Order-Management-System');
   });
 });
