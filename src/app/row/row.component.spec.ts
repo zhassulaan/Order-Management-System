fbb42319db0c0af5e3a353e0c74a196c4f29884a
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-05-20 11:49:13 +0500

    wqd

--- src/app/row/row.component.spec.ts
+++ src/app/row/row.component.spec.ts
@@ -1,23 +1,23 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { RowComponent } from './row.component';
+import { RowComponent } from './row.component';
 
-// describe('RowComponent', () => {
-//   let component: RowComponent;
-//   let fixture: ComponentFixture<RowComponent>;
+describe('RowComponent', () => {
+  let component: RowComponent;
+  let fixture: ComponentFixture<RowComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [RowComponent]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RowComponent]
+    })
+    .compileComponents();
     
-//     fixture = TestBed.createComponent(RowComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(RowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
